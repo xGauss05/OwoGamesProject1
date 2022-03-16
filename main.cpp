cccc71fc8d9d7a97ae5de5e808b6fd2f948b0bc9
xGauss05 <jcacaynes@gmail.com>
2022-03-16 14:20:07 +0100

    Included libraries in order for SDL to work.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+#include "SDL/include/SDL.h"
+#include "SDL_image/include/SDL_image.h"
+#include "SDL_mixer/include/SDL_mixer.h"
+#pragma comment( lib, "SDL/libx86/SDL2.lib" )
+#pragma comment( lib, "SDL/libx86/SDL2main.lib" )
+#pragma comment( lib, "SDL_image/libx86/SDL2_image.lib" )
+#pragma comment( lib, "SDL_mixer/libx86/SDL2_mixer.lib" )
+
 int main(int argc, char* args[]) {
 	cout << "Hello world" << endl;
 	return 0;
