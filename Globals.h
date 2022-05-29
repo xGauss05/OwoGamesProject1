112ab72196b9ac5e48991fca2e973bcbbe83b421
Ethanm-0371 <ethanm0371@gmail.com>
2022-05-29 19:00:25 +0200

    Fixed camera jitter + every other issue regarding SCREEN_SIZE. From now on, there is no need to change the constant. Changing from windowed to fullscreen can now be done by only changing WIN_FULLSCREEN_DESKTOP to either 1 or 0. The changes made in this commit take care of the rest.

--- Globals.h
+++ Globals.h
@@ -23,7 +23,7 @@ typedef unsigned short ushort;
 #define SCREEN_WIDTH 224
 #define SCREEN_HEIGHT 384
 
-#define SCREEN_SIZE 1
+#define SCREEN_SIZE 2
 #define WIN_FULLSCREEN 0
 #define WIN_FULLSCREEN_DESKTOP 1
 #define WIN_BORDERLESS 0
